36b97c7e351301b71d5b1645b50cdd7e9eb70f1d
Aaron Huff <aaron.t.huff@gmail.com>
2022-04-18 15:00:51 -0400

    Update casks.sh

--- casks.sh
+++ casks.sh
@@ -1,8 +1,8 @@
 ## declare an array variable
 ## can be on the same line or on multi-lines
-declare -a arr=("zsh" "vscodium" "discord" "htop" "audius" "authy" "bitwarden" "brave-browser" 
+declare -a arr=("zsh" "vscodium" "discord" "htop" "authy" "bitwarden" "brave-browser" 
   "docker" "google-drive" "google-chrome" "grammarly" "grammarly-desktop" "nordvpn" "notion"
-  "powershell" "spotify" "vlc"
+  "spotify" "vlc"
 )
 
 ## now loop through the above array with use of the loop-variable `i`
