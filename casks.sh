77c0f55b69bb0c5bfed0437311aba333385e8b31
Aaron Huff <aaron.t.huff@gmail.com>
2022-04-18 15:02:18 -0400

    Update casks.sh

--- casks.sh
+++ casks.sh
@@ -1,12 +1,11 @@
-## declare an array variable
-## can be on the same line or on multi-lines
-declare -a arr=("zsh" "vscodium" "discord" "htop" "authy" "bitwarden" "brave-browser" 
+## declare casks array variable
+declare -a casks=("zsh" "vscodium" "discord" "htop" "authy" "bitwarden" "brave-browser" 
   "docker" "google-drive" "google-chrome" "grammarly" "grammarly-desktop" "nordvpn" "notion"
   "spotify" "vlc"
 )
 
-## now loop through the above array with use of the loop-variable `i`
-for i in "${arr[@]}"
+## loop array to install casks
+for i in "${casks[@]}"
 do
    echo "Installing $i..."
    brew install $i
